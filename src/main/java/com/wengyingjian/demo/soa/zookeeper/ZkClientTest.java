ee65baea8cc68da2314ea3bb274c8ff05596b35c
wengyingjian <q291611265@gmail.com>
2016-03-10 13:19:07 +0800

    zkClient监听器

--- src/main/java/com/wengyingjian/demo/soa/zookeeper/ZkClientTest.java
+++ src/main/java/com/wengyingjian/demo/soa/zookeeper/ZkClientTest.java
@@ -1,7 +1,11 @@
 package com.wengyingjian.demo.soa.zookeeper;
 
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.IZkStateListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.Watcher;
 
 import java.util.List;
 
@@ -19,6 +23,13 @@ public class ZkClientTest {
         CreateMode acl = CreateMode.EPHEMERAL;
 
         ZkClient zkClient = new ZkClient(serverList);
+
+        //监听path路径下子节点变化
+        zkClient.subscribeChildChanges(path, new MyChildChangeListener());
+        //监听path路径下数据变化
+        zkClient.subscribeDataChanges(path, new MyDataChangeListener());
+        //监听连接的状态
+        zkClient.subscribeStateChanges(new MyStateChangeListener());
 
         //删除节点
         boolean isDeleted = zkClient.delete(path);
@@ -62,4 +73,82 @@ public class ZkClientTest {
 
     }
 
+    /**
+     * 子节点状态发生变化监听器
+     */
+    static class MyChildChangeListener implements IZkChildListener {
+
+        /**
+         * 当指定路径的子节点发生变化时,会调用以下方法
+         *
+         * @param parentPath
+         * @param currentChilds
+         * @throws Exception
+         */
+        @Override
+        public void handleChildChange(String parentPath, List<String> currentChilds) throws Exception {
+
+        }
+    }
+
+    /**
+     * 节点数据发生变化监听器
+     */
+    static class MyDataChangeListener implements IZkDataListener {
+
+        @Override
+        public void handleDataChange(String dataPath, Object data) throws Exception {
+
+        }
+
+        @Override
+        public void handleDataDeleted(String dataPath) throws Exception {
+
+        }
+    }
+
+    /**
+     * 连接状态发生变化监听器
+     */
+    static class MyStateChangeListener implements IZkStateListener {
+
+        /**
+         * zookeeper连接状态发生变化的时候会调用以下方法
+         *
+         * @param state
+         * @throws Exception
+         */
+        @Override
+        public void handleStateChanged(Watcher.Event.KeeperState state) throws Exception {
+
+        }
+
+        /**
+         * 当zookeeper旧的session过期了,新的session创建的时候会调用以下方法.
+         * <p>
+         * 需要做的:
+         * 重建 EPHEMERAL 模式的连接
+         *
+         * @throws Exception
+         * @see CreateMode#EPHEMERAL
+         */
+        @Override
+        public void handleNewSession() throws Exception {
+
+        }
+
+        /**
+         * 当session无法被重新创建(重新创建失败)的时候调用以下的方法
+         * <p>
+         * 需要做的:
+         * 做好连接失败异常处理,比如:重新建立连接,报告异常...
+         *
+         * @param error
+         * @throws Exception
+         */
+        @Override
+        public void handleSessionEstablishmentError(Throwable error) throws Exception {
+
+        }
+    }
 }
